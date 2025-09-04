db6b66a3039d765c9d550ba24cf193b15f2b3bc3
Andrew <Andrew@BB21312>
2025-09-04 17:14:34 -0500

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    
+    
+    internal class Program
+    {
+        //method for updating correct guesses
+        static void UpdateGuesses(Word myWord, char guess)
+        {
+            if (myWord.Name.Contains(guess))
+            {
+                if (myWord.CorrectGuesses != null)
+                {
+                    foreach (char letter in myWord.CorrectGuesses)
+                    {
+                        if (myWord.CorrectGuesses.Contains(guess))
+                        {
+                            Console.WriteLine("You already guessed that letter!");
+                            return;
+                        }
+                        else
+                        {
+                            myWord.CorrectGuesses += guess;
+                        }
+                    }
+                }
+                else
+                {
+                    myWord.CorrectGuesses += guess;
+                }
+            }
+
+            else
+            {
+                myWord.Misses += 1;
+            }
+
+            
+        }
+        //method for one word hangman
+        static void OneWordHang()
+        {
+            Word myWord = new Word();
+            
+            //enter loop until user guesses word or runs out of tries
+            while (true)
+            {
+                char guess;
+                myWord.HideWord();
+                Console.WriteLine($"{myWord.Hidden}\nEnter Guess:");
+                guess = Convert.ToChar(Console.ReadLine());
+                UpdateGuesses(myWord, guess);
+                Console.WriteLine(myWord.CorrectGuesses);
+                Console.WriteLine(myWord.Name);
+                Console.WriteLine(guess);
+                Console.WriteLine(myWord.Hidden);
+            }
+
+            Console.WriteLine($"Word is {myWord.Name}");
+        }
+
+
+        //method for array of words
+        static void Main(string[] args)
+        {
+            //enter loop until user wants to exit
+            while (true)
+            {
+                Console.WriteLine("Menu:\n1. One Word Hangman\n2. Array of Words Hangman\n3. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                //switch to call methods
+                switch (choice)
+                {
+                    case 1:
+                        OneWordHang();
+                        break;
+                    case 2:
+                        //ArrayOfWordsHang();
+                        break;
+                    case 3:
+                        Console.WriteLine("Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Hangman/Word.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Hangman
+{
+    internal class Word
+    {
+        private string correctGuesses;
+        private string name;
+        private string hidden;
+        private int length;
+        private int misses;
+
+        public Word()
+        {
+            name= "sequence";
+        }
+        
+        public string CorrectGuesses
+        {
+            get { return correctGuesses; }
+            set { correctGuesses = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Hidden
+        {
+            get { return hidden; }
+            set { hidden = value; }
+        }
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public int Misses
+        {
+            get { return misses; }
+            set { misses += value; }
+        }
+
+        public void HideWord()
+        {
+            hidden = "";
+            if (correctGuesses != null)
+            {
+                foreach (char letter in name)
+                {
+                    foreach (char let in correctGuesses)
+                    {
+                        if (letter == let)
+                        {
+                            hidden += letter;
+                        }
+                        else if (letter == ' ')
+                        {
+                            hidden += " ";
+                        }
+                        else
+                        {
+                            hidden += "_";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (char letter in name)
+                {
+                    if (letter == ' ')
+                    {
+                        hidden += " ";
+                    }
+                    else
+                    {
+                        hidden += "_";
+                    }
+                }
+            }
+        }
+    }
+}
